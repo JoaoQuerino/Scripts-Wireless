e0b629b3962eb43bd4c1d1bb6bb01785b1436713
Joao Querino dos Reis Neto <joaoreis1916@gmail.com>
2023-08-05 17:42:29 -0300

    Start of Sniffer refactoring
    Co-Authored-By: Joao Luiz Cardoso Junior <59375606+JoaoLuizCJ@users.noreply.github.com>

--- Sniffer/Sniffer.py
+++ Sniffer/Sniffer.py
@@ -1,82 +1,85 @@
+from pickle import PROTO
+from typing import Protocol
 from scapy.all import *
-from scapy.all import IP
+from scapy.all import IP, TCP, UDP, ICMP
 import time
-import sys
 from termcolor import colored
 import os
 
 
-def cadaPktGeral(packet): 
+def cadaPktGeral(packet):
+    print('Entrou no print pkt geral')
     print(colored("*******RESUMO DO PACOTE*******", 'white', attrs=['bold']))
     print(packet.summary())
     print()
     print(colored("*******DETALHES DO PACOTE*******", 'white', attrs=['bold']))
     print(packet.show())
-    print(colored("*******FIM DO PACOTE*******\n\n", 'white', attrs=['bold']))
-    if pcap_arquivo and pcap_caminho:
-        wrpcap(pcap_caminho + "/" + pcap_arquivo, packet, append=True)
+    print(colored("*******FIM DO PACOTE*******\n\n", 'white', attrs=['bold']))        
 
-def cadaPkt(packet):
-    global contador #TODO: não usar variável global
-    if packet.haslayer(IP) and (packet[IP].src == alvo or packet[IP].dst == alvo):
-        cadaPktGeral(packet)
-    else:
-        contador -= 1
-    # Se o nome do arquivo de captura e o caminho estiverem definidos, salve o pacote no arquivo
+def cada_pkt_geral_salva(packet):
+    global pcap_arquivo
+    cadaPktGeral(packet)
+    wrpcap(os.path.dirname(os.path.abspath(__file__)) + "/" + pcap_arquivo, packet, append=True)
 
-
-if __name__ == "__main__":
-
-    placaDeRede = 'wlxf4ec38924d47' 
-    alvo = input("Informe um endereço IPV4 local para estar sendo analisado, ou deixe em branco para analisar a rede toda: ")
-    
-#: TODO: criar método para escolher opções de salvamento
+def choose_saving_options():
     salvar = input("Deseja que a leitura seja salva? S para sim ou outra tecla para nao\n")
     if salvar.lower() == "s":
-        pcap_arquivo = input("Informe o nome do arquivo pcap a ser salvo: ")
-        pcap_caminho = input("Informe o caminho de salvamento do arquivo PCAP ou deixe em branco para salvar no local do script: ")
-        if pcap_caminho == "":
-            #Pega caminho do script
-            pcap_caminho = os.path.dirname(os.path.abspath(__file__))      
+        pcap_arquivo = input("informe o nome do arquivo pcap a ser salvo: ")
+        # TODO: não aceitar entrada vazia
     else:
         pcap_arquivo = None
-        pcap_caminho = None
-            
+    return pcap_arquivo
 
-#TODO: inportar método para selecionar protocolo (protocol_input)
+def choose_protocol():
     protocolo = input("Gostaria de filtrar um protocolo? S para sim ou outra tecla para não: ")
-
     if protocolo.lower() == "s":
         while True:
             protocolo_opcao = input("Qual protocolo gostaria de filtrar? 1 = ICMP || 2 = TCP || 3 = UDP ")
-            if protocolo_opcao == "1":
-                protocolo = "icmp"
-                break
-            elif protocolo_opcao == "2":
-                protocolo = "tcp"
-                break
-            elif protocolo_opcao == "3":
-                protocolo = "udp"
-                break
+            if protocolo_opcao in ["1", "2", "3"]:
+                if protocolo_opcao == "1":
+                    return "icmp"
+                elif protocolo_opcao == "2":
+                    return "tcp"
+                else:
+                    return "udp"
             else:
                 print("Protocolo inválido. Por favor, Informe 1, 2 ou 3.")
-    else:
-        protocolo = ""
+    return ""
 
+def get_packet_count():
+    while True:
+        try:
+            contador = int(input("Informe o numero de pacotes que deseja analisar: "))
+            if contador > 0:
+                return contador
+            else:
+                print("Informe um número positivo maior que zero.")
+        except ValueError:
+            print("Informe um valor inteiro válido.")
 
+if __name__ == "__main__":
+    placaDeRede = 'Wi-Fi'
+    alvo = input("Informe um endereço IPV4 local para estar sendo analisado, ou deixe em branco para analisar a rede toda: ")
 
-    contador = int(input("Informe o numero de pacotes que deseja analisar: ")) #TODO: validar - inteiro positivo
+    pcap_arquivo = choose_saving_options()
+    protocolo = choose_protocol()
+    contador = get_packet_count()
+
+    t1 = time.time()
+    filtro = protocolo
+    if alvo: filtro += f'{"" if not protocolo else " and "}host {alvo}'
 
     
-    t1 = time.time()
-    filtro = ""
-    if protocolo:
-        filtro += protocolo
     print("Leitura iniciada...")
-    if alvo != "":
-        sniff(iface=placaDeRede, prn=cadaPkt, count=contador, store=0, filter=filtro)
+    print(alvo, pcap_arquivo)
+    if pcap_arquivo:
+        print(1)
+        sniff(iface=placaDeRede, prn=cada_pkt_geral_salva, count=contador, store=0, 
+              filter=filtro)
     else:
+        print(2)
         sniff(iface=placaDeRede, prn=cadaPktGeral, count=contador, store=0, filter=filtro)
+
     t2 = time.time()
     total = t2 - t1 
     print(f"Tempo para leitura: {total}")
