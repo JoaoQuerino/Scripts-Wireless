b1b636763a92d635a8fb7399dfade84eaec6c7f4
Joao Querino dos Reis Neto <joaoreis1916@gmail.com>
2023-08-11 23:08:23 -0300

    Start anomaly monitor
    start of translating functions, variables and strings
    
    Co-Authored-By: Joao Luiz Cardoso Junior <59375606+JoaoLuizCJ@users.noreply.github.com>

--- anomaly_monitor.py
+++ anomaly_monitor.py
@@ -9,32 +9,32 @@ from printer import FontTypes, formater_text, f_input, f_print
 
 
 # TODO: Padronizar lingua, usar modulos próprios, aplicar boas praticas como saida esperada, identação e nomes de funções/variaveis
-monitorado_ip = host_ip_input()
+monitored_ip = host_ip_input()
 while(True):
     try:
         
-        maxPkt = int(input("Informe o número máximo de pacotes para aviso: "))
-        intConf = int(input("Informe o tempo de intervalo entre cada análise em segundos: "))
-        duracao = int(input("Informe a duração da execução em segundos: "))
+        maximum_to_alert = int(input("Enter the maximum number of packages for notice: "))
+        interval_in_seconds = int(input("Enter the interval time between each analysis in seconds: "))
+        duration = int(input("Enter duration of execution in seconds: "))
         break
     except:
         f_print(formater_text('Input processing error', FontTypes.ALERT))
 
-periodo = datetime.timedelta(seconds=duracao)
+period = datetime.timedelta(seconds=duration)
 
-cntRec = 0
-cntTurno = 0
+received_counter = 0
+read_counter = 0
 
-turno = []
-quantidade = []
-listaRepet = []
-culpados = []
-dadosTabela = []
-dadosNovosTabela = []
+reading = []
+quantity_of_packages = []
+send_counter_by_source = []
+source_list = []
+table_data = []
+new_table_data = []
 
 while True:
-    salvar = input("Deseja que a captura seja salva? S para sim ou qualquer outra tecla para não: ")
-    if salvar.lower() == "s":
+    save = input("Deseja que a captura seja salva? S para sim ou qualquer outra tecla para não: ")
+    if save.lower() == "s":
         arquivoNome = input("Informe o nome do arquivo a ser salvo: ")
         arquivoCaminho = input("Informe o caminho completo do local onde o arquivo será salvo ou deixe em branco para salvar no local do script: ")
         if arquivoCaminho != "":
@@ -50,29 +50,29 @@ while True:
         break
 
 # Função para contar o número de pacotes recebidos por um dispositivo
-def cadaPacote(packet):
-    global cntRec
-    if packet.haslayer(IP) and packet[IP].src != monitorado_ip:
-        global listaRepet
-        listaRepet.append(packet)
-    if packet.haslayer(IP) and packet[IP].dst == monitorado_ip:
-        cntRec += 1
+def packet_handler(packet):
+    global received_counter
+    if packet.haslayer(IP) and packet[IP].src != monitored_ip:
+        global send_counter_by_source
+        send_counter_by_source.append(packet)
+    if packet.haslayer(IP) and packet[IP].dst == monitored_ip:
+        received_counter += 1
 
-validaLimite = False
+check_limit = False
 
-prmMax = True
-print(tabulate(dadosTabela, headers=["Leitura", " IP DST", "PACOTES RECEBIDOS", "IP SRC", "MAX PACOTES", "HORA DA LEITURA"], tablefmt="grid"))
-inicio = datetime.datetime.now()
-fim = inicio + periodo
+first_maximum = True
+print(tabulate(table_data, headers=["Leitura", " IP DST", "PACOTES RECEBIDOS", "IP SRC", "MAX PACOTES", "HORA DA LEITURA"], tablefmt="grid"))
+start_monitoring = datetime.datetime.now()
+stop_monitoring = start_monitoring + period
 
-while datetime.datetime.now() < fim:
+while datetime.datetime.now() < stop_monitoring:
     # Inicie a captura de pacotes na rede local para o dispositivo monitorado      
     try:      
-        cntTurno += 1
-        sniff(prn=cadaPacote, filter="ip", iface="wlxf4ec38924d47", timeout=intConf)
+        read_counter += 1
+        sniff(prn=packet_handler, filter="ip", iface="Wi-Fi", timeout=interval_in_seconds)
         # Verifique se o número de pacotes recebidos pelo dispositivo excedeu o limite
         hosts = {}
-        for pkt in listaRepet:
+        for pkt in send_counter_by_source:
             if pkt.haslayer(IP):
                 ip_src = pkt[IP].src
                 if ip_src in hosts:
@@ -81,40 +81,40 @@ while datetime.datetime.now() < fim:
                     hosts[ip_src] = 1
 
         for hosts, count in hosts.items():
-            if count > maxPkt:
-                culpados.append(hosts)
+            if count > maximum_to_alert:
+                source_list.append(hosts)
 
-        if cntRec > maxPkt:
-            validaLimite = True
-            string_sem_colchetes = ' '.join(str(elemento) for elemento in culpados)
-            agora = datetime.datetime.now()
-            dadosNovosTabela = [cntTurno, monitorado_ip, cntRec, string_sem_colchetes, maxPkt, agora]
-            print(tabulate([[colored(item, 'red') if item == cntRec else item for item in dadosNovosTabela]], tablefmt="grid"))
-            culpados.clear()
+        if received_counter > maximum_to_alert:
+            check_limit = True
+            string_without_brackets = ' '.join(str(element) for element in source_list)
+            reading_moment = datetime.datetime.now()
+            new_table_data = [read_counter, monitored_ip, received_counter, string_without_brackets, maximum_to_alert, reading_moment]
+            print(tabulate([[colored(item, 'red') if item == received_counter else item for item in new_table_data]], tablefmt="grid"))
+            source_list.clear()
             if arquivoCaminho and arquivoNome:
                 with open(infSalv, "a") as file:
-                    if prmMax == True:
+                    if first_maximum == True:
                         file.write("Destino;Origem;Numero de pacotes;Segundo(s);Quantidade Maxima;Hora da Leitura\n")
-                    file.write(f"{monitorado_ip};{string_sem_colchetes};{cntRec};{intConf};{maxPkt};{agora}\n")
-                    prmMax = False
+                    file.write(f"{monitored_ip};{string_without_brackets};{received_counter};{interval_in_seconds};{maximum_to_alert};{reading_moment}\n")
+                    first_maximum = False
         else:
-            culpados.clear()
-            agora = datetime.datetime.now()
-            dadosNovosTabela = [cntTurno, monitorado_ip, cntRec, "SEM SUSPEITOS", maxPkt, agora]
-            print(tabulate([dadosNovosTabela], tablefmt="grid"))          
+            source_list.clear()
+            read_time = datetime.datetime.now()
+            new_table_data = [read_counter, monitored_ip, received_counter, "SEM SUSPEITOS", maximum_to_alert, read_time]
+            print(tabulate([new_table_data], tablefmt="grid"))          
 
-        quantidade.append(cntRec)
-        cntRec = 0
-        turno.append(cntTurno)
+        quantity_of_packages.append(received_counter)
+        received_counter = 0
+        reading.append(read_counter)
     except KeyboardInterrupt:
-        print("Erro de processamento ou parada forçada")
-    except:
-        print("Erro de processamento ou parada forçada")
+        print('forced stop')
+    except Exception as e:
+        print("Processing error: " + str(e))
 
-plt.plot(turno, quantidade)
+plt.plot(reading, quantity_of_packages)
 
-plt.title('Quantidade de leituras por turno')
-plt.xlabel('Por segundo(s)')
-plt.ylabel('Quantidade de pacotes')
+plt.title('Number of readings per shift')
+plt.xlabel('per second(s)')
+plt.ylabel('quantity of packages')
 
 plt.show()
